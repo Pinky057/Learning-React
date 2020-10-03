fcc13f6b2d81334a4f4a38f4dfa466bc21664a6f
Pinky057 <ishratjahanpinky2@gmail.com>
2020-10-03 09:04:23 +0600

    select option using useeffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default function UserStorage () {
+  const [ user, setUser ] = useState("Sanjiv");
+
+  useEffect(() => {
+    const storedUser = window.localStorage.getItem("user");
+
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("user", user);
+  }, [user]);
+
+  return (
+      <select style={{padding: 60, marginLeft:700, marginTop:60, fontSize:20}} value={user} onChange={e => setUser(e.target.value)}>
+          <option style={{fontSize:20, padding:20}}>RICE</option>
+          <option style={{fontSize:20, padding:20}}>CHICKEN</option>
+          <option style={{fontSize:20, padding:20}}>PIZZA</option>
+          <option style={{fontSize:20, padding:20}}>CHESS</option>
+          <option style={{fontSize:20, padding:20}}>PASTA</option>
+          <option style={{fontSize:20, padding:20}}>PIZZA</option>
+          <option style={{fontSize:20, padding:20}}>CHESS</option>
+      </select>
+  );
+};
+
