87ef922271814b72689bc020dddbbb61ba069522
Pinky057 <ishratjahanpinky2@gmail.com>
2020-10-04 08:49:30 +0600

    memo example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState, useCallback, useMemo} from 'react';
 import './App.css';
+import Child from "./components/child";
 
 function App() {
-  const[count, changeCount]=useState(0);
+  const [i, setI]= useState(0);
+  function onClickHandle(){
+    setI(i +1)
+  }
+  const memoChild = useMemo(()=>{
+    return <Child></Child>
+  },[]);
+
   return (
-      <div class="counter">
-      <h2>{count}</h2>
-<button onClick={()=>changeCount(count - 1)}><span>-1</span></button>
-<button onClick={()=>changeCount(count + 1)}><span>+1</span></button>
-<button onClick={()=>changeCount(count === 0)}><span>Reset</span></button>
-</div>
+    <div className="App">
+      <header className="App-header">
+        <h3>Use Memo</h3>
+        <h2>i: {i}</h2>
+        <button onClick={onClickHandle}>Increment I</button>
+        <h3> normal render</h3>
+        <child></child>
+        <h3> Memo Render</h3>
+        {memoChild}
+      </header>
+    </div>
   );
 }
 
