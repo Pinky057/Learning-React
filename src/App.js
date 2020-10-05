45dc6493011e2667b30946fa7a2b6205876c1f4c
Pinky057 <ishratjahanpinky2@gmail.com>
2020-10-05 07:56:04 +0600

    fibonacci using usememo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React,{useState} from 'react';
+import React, { useMemo, useState } from "react";
+
 import './App.css';
 
 function App() {
-  const[count, changeCount]=useState(0);
-  return (
-      <div class="counter">
-      <h2>{count}</h2>
-<button onClick={()=>changeCount(count - 1)}><span>-1</span></button>
-<button onClick={()=>changeCount(count + 1)}><span>+1</span></button>
-<button onClick={()=>changeCount(count === 0)}><span>Reset</span></button>
-</div>
-  );
-}
+    const [fibLength, setFibLength] = useState(3);
+    const [count, setCount] = useState(0);
 
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const fibonacciSequence = useMemo(() => {
+        const t1 = performance.now();
+        const result = [1, 1];
+        for (let i = 2; i < fibLength; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        const t2 = performance.now();
+        console.log(`Fibonacci calculation took ${t2 - t1}ms`);
+        return result;
+    }, [fibLength]);
+
+    return (
+        <>
+            <button style={{ margin:30, paddingLeft:20, paddingRight:20 }} onClick={increment}> { count } </button>
+            <hr />
+            <input style={{margin:20}}
+                   value = { fibLength }
+                onChange = {(e) => setFibLength(Number(e.target.value))}
+            />
+            <p style={{margin:30}}>
+                Fibonacci sequence of length {fibLength}: {fibonacciSequence.join(", ")}
+            </p>
+        </>
+    );
+}
 export default App;
